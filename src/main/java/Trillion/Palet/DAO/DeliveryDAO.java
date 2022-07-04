dfd96d0ab6660eb68cd31b7c38297e37da026f83
LHG <i2376@naver.com>
2022-07-04 10:01:52 +0900

    delivery

---
+++ src/main/java/Trillion/Palet/DAO/DeliveryDAO.java
@@ -0,0 +1,65 @@
+package Trillion.Palet.DAO;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import Trillion.Palet.DTO.DeliveryDTO;
+
+
+@Repository
+public class DeliveryDAO {
+
+		@Autowired
+		private SqlSession mybatis;
+
+		public DeliveryDTO selectDefaultAddress(String email) {
+			return mybatis.selectOne("Delivery.selectDefaultAddress",email);
+		}
+
+		public int insertNewDefaultAddress(DeliveryDTO dto) {
+			String email = dto.getEmail();
+			System.out.println(email);
+			mybatis.update("Delivery.updateDefaultAddress",email);
+			return mybatis.insert("Delivery.insertNewAddress",dto);
+		}
+		public int insertNewAddress(DeliveryDTO dto) {
+			
+			return mybatis.insert("Delivery.insertNewAddress",dto);
+		}
+
+		public List<DeliveryDTO> chooseAddress(String email) {
+			System.out.println("dao"+email);
+			return mybatis.selectList("Delivery.chooseAddress",email);
+		}
+
+		public DeliveryDTO selectAddress(int deliveryaddress_seq, String email) {
+			Map<Object,Object> param = new HashMap<>();
+			param.put("email",email);
+			param.put("deliveryaddress_seq",deliveryaddress_seq);
+			
+			return mybatis.selectOne("Delivery.selectAddress",param);
+		}
+
+		public int delAddress(int deliveryaddress_seq) {
+			return mybatis.delete("Delivery.delAddress",deliveryaddress_seq);
+		}
+
+		public int updateAddress(DeliveryDTO dto) {
+			System.out.println(dto.getName());
+			System.out.println(dto.getDefaultaddress());
+			System.out.println(dto.getReceiver());
+			System.out.println(dto.getPostcode());
+			System.out.println(dto.getAddress1());
+			System.out.println(dto.getAddress2());
+			System.out.println(dto.getPhone());
+			System.out.println(dto.getEmail());
+			System.out.println(dto.getDeliveryaddress_seq());
+			return mybatis.update("Delivery.updateAddress",dto);
+		}
+		
+}
