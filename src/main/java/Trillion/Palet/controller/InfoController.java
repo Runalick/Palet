08bb04bedc19c879b6aea6352d452a36f33ee40b
LHG <i2376@naver.com>
2022-07-09 21:50:47 +0900

    about

--- src/main/java/Trillion/Palet/controller/InfoController.java
+++ src/main/java/Trillion/Palet/controller/InfoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import Trillion.Palet.DTO.FaqDTO;
 import Trillion.Palet.service.InfoService;
@@ -52,5 +53,17 @@ public class InfoController {
 		
 		iserv.faqInsert(dto);
 		return "redirect:email";
+	}
+	
+	//대관 문의하기
+	@ResponseBody
+	@RequestMapping("askEmail")
+	public String askEmail(String userEmail,String title ,String contents) {
+		
+		System.out.println(userEmail);
+		System.out.println(title);
+		System.out.println(contents);
+		iserv.userEmail(userEmail, title, contents);
+		return "success";
 	}
 }
