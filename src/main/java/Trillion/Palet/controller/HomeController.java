73bd4d9d59511734316988d20cb462ce7f044fd2
LHG <i2376@naver.com>
2022-07-20 16:27:05 +0900

    home

--- src/main/java/Trillion/Palet/controller/HomeController.java
+++ src/main/java/Trillion/Palet/controller/HomeController.java
@@ -1,5 +1,7 @@
 package Trillion.Palet.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +9,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import Trillion.Palet.DTO.GoodsDTO;
+import Trillion.Palet.service.GoodsService;
+
 @Controller
 public class HomeController {
 	
 	@Autowired
 	private HttpSession session;
+	@Autowired
+	private GoodsService gServ;
 	
 	
 	@RequestMapping(value = "/")
 	public String home(Model model) {
 		String loginEmail = (String) session.getAttribute("loginEmail");
 		model.addAttribute("loginEmail", loginEmail);
+		
+		//굿즈 탑6
+		List<GoodsDTO> dto = gServ.gettop6goods();
+		model.addAttribute("dto",dto);
 		return "home";
 	}
 	
--- src/main/java/Trillion/Palet/service/GoodsService.java
+++ src/main/java/Trillion/Palet/service/GoodsService.java
@@ -74,5 +74,9 @@ public class GoodsService {
 	public List<GoodsDTO> getoption(int g_num) {
 		return dao.getoption(g_num);
 	}
+
+	public List<GoodsDTO> gettop6goods() {
+		return dao.gettop6goods();
+	}
 	
 }
